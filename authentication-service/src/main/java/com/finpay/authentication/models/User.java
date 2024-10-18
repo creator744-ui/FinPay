dcb2e5f5a1a506f6a83c0591c78247891643a5b1
felixojiambo <felixojiamboe@gmail.com>
2024-10-18 23:27:13 +0300

    User entity to represent users in the system.

---
+++ authentication-service/src/main/java/com/finpay/authentication/models/User.java
@@ -0,0 +1,29 @@
+package com.finpay.authentication.models;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+
+}
