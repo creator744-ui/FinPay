b4b38682b823df9eda703d3799e9d8eec6a0bbef
felixojiambo <felixojiamboe@gmail.com>
2024-10-18 23:22:36 +0300

    initial setup

---
+++ authentication-service/src/main/java/com/finpay/authentication/AuthenticationApplication.java
@@ -0,0 +1,9 @@
+package com.finpay.authentication;
+
+public class AuthenticationApplication {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("User Auth");
+    }
+}
