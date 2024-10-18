8988b8c73e668d4f8afb9fe936057ea243500405
felixojiambo <felixojiamboe@gmail.com>
2024-10-18 23:40:38 +0300

    Create a service to load user-specific data.

---
+++ authentication-service/src/main/java/com/finpay/authentication/services/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.finpay.authentication.services;
+
+import com.finpay.authentication.models.User;
+import com.finpay.authentication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User Not Found with email: " + email));
+
+        // For simplicity, all users have ROLE_USER
+        List<GrantedAuthority> authorities =
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
+    }
+}
