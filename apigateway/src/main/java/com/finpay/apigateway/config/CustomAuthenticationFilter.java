cd126fde278f05198c2b711e80f752828d19f8be
felixojiambo <felixojiamboe@gmail.com>
2024-10-19 14:19:56 +0300

    helm

---
+++ apigateway/src/main/java/com/finpay/apigateway/config/CustomAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.finpay.apigateway.config;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class CustomAuthenticationFilter extends AbstractGatewayFilterFactory<CustomAuthenticationFilter.Config> {
+
+    public static class Config {
+
+    }
+
+    public CustomAuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            return chain.filter(exchange);
+        };
+    }
+}
