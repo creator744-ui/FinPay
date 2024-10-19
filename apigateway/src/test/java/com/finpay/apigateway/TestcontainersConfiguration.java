f499767b1a6e2929fe299196f5399dca97d3d6ba
felixojiambo <felixojiamboe@gmail.com>
2024-10-19 14:09:19 +0300

    api gateway setup

---
+++ apigateway/src/test/java/com/finpay/apigateway/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.finpay.apigateway;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
