2403d4d058489d1b1dc1c999493b051bd7c470fa
felixojiambo <felixojiamboe@gmail.com>
2024-10-20 08:23:09 +0300

    mapping within the Role enum to associate permissions.

--- user-service/src/main/java/com/finpay/user_service/models/Role.java
+++ user-service/src/main/java/com/finpay/user_service/models/Role.java
@@ -1,7 +1,19 @@
 package com.finpay.user_service.models;
+
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
-    ROLE_MANAGER
-    // Add additional roles as needed
+    ROLE_USER(Set.of(Permission.READ_USER)),
+    ROLE_ADMIN(Set.of(Permission.READ_USER, Permission.WRITE_USER, Permission.DELETE_USER, Permission.READ_INVOICE, Permission.WRITE_INVOICE, Permission.DELETE_INVOICE)),
+    ROLE_MANAGER(Set.of(Permission.READ_USER, Permission.WRITE_USER, Permission.READ_INVOICE, Permission.WRITE_INVOICE));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
 }
