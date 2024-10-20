f5f1e9a76d3d9cbfa6669eb9fcedebb9b6e0f80a
felixojiambo <felixojiamboe@gmail.com>
2024-10-20 08:20:21 +0300

    userservice

---
+++ user-service/src/main/java/com/finpay/user_service/models/Permission.java
@@ -0,0 +1,11 @@
+package com.finpay.user_service.models;
+
+public enum Permission {
+    READ_USER,
+    WRITE_USER,
+    DELETE_USER,
+    READ_INVOICE,
+    WRITE_INVOICE,
+    DELETE_INVOICE,
+    // Add additional permissions as needed
+}
---
+++ user-service/src/main/java/com/finpay/user_service/models/Role.java
@@ -0,0 +1,7 @@
+package com.finpay.user_service.models;
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_MANAGER
+    // Add additional roles as needed
+}
