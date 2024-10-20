adc629cb2cf1d4ed0e20293a75311e8f1e9eaa3e
felixojiambo <felixojiamboe@gmail.com>
2024-10-20 08:21:44 +0300

    users entity

---
+++ user-service/src/main/java/com/finpay/user_service/models/User.java
@@ -0,0 +1,33 @@
+package com.finpay.user_service.models;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Set<Role> roles;
+}
