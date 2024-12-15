ff2dda433d7a828f88e383b020fd1e2a93d13360
felixojiambo <felixojiamboe@gmail.com>
2024-12-15 20:24:50 +0300

    models complete, invoice to be managed via service-to-service communication without creating an entity in the Payment Service.

---
+++ payment-service/src/main/java/com/finpay/payment_service/models/PaymentGateway.java
@@ -0,0 +1,30 @@
+package com.finpay.payment_service.models;
+
+import lombok.*;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payment_gateways")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentGateway {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // e.g., Stripe, PayPal, Square
+
+    @Column(nullable = false)
+    private String apiEndpoint;
+
+    @Column(nullable = false)
+    private String apiKey;
+
+    @OneToMany(mappedBy = "paymentGateway", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Payment> payments;
+}
---
+++ payment-service/src/main/java/com/finpay/payment_service/models/RefundStatus.java
@@ -0,0 +1,7 @@
+package com.finpay.payment_service.models;
+
+public enum RefundStatus {
+    INITIATED,
+    COMPLETED,
+    FAILED
+}
