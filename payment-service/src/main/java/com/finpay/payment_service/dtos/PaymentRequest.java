88cf5f4c8352c780233b16f79faf46ad0b2e6ad5
felixojiambo <felixojiamboe@gmail.com>
2024-12-30 20:02:00 +0300

    provide contructor

--- payment-service/src/main/java/com/finpay/payment_service/dtos/PaymentRequest.java
+++ payment-service/src/main/java/com/finpay/payment_service/dtos/PaymentRequest.java
@@ -1,13 +1,17 @@
 package com.finpay.payment_service.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequest {
     private BigDecimal amount;
     private String currency;
