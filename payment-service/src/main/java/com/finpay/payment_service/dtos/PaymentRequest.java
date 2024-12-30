8798cacd72fe6ae551261ee3753bb3a2d62e356f
felixojiambo <felixojiamboe@gmail.com>
2024-12-30 19:58:26 +0300

    PaymentRequest.builder()

--- payment-service/src/main/java/com/finpay/payment_service/dtos/PaymentRequest.java
+++ payment-service/src/main/java/com/finpay/payment_service/dtos/PaymentRequest.java
@@ -1,15 +1,17 @@
 package com.finpay.payment_service.dtos;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Data
+@Builder
 public class PaymentRequest {
     private BigDecimal amount;
     private String currency;
-    private String paymentMethod; // e.g., CREDIT_CARD, BANK_TRANSFER
-    private UUID invoiceId; // Reference to Invoice managed by Invoice Service
-    private String paymentGateway; // e.g., Stripe, PayPal
+    private String paymentMethod;
+    private UUID invoiceId;
+    private String paymentGateway;
 }
